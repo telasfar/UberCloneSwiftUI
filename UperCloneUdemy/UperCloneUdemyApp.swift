3ef6ecf69e0581a46bb1b7ca038d10ce13ffdb9a
tariq.maged <tariq.maged@vodafone.com>
2025-10-08 15:15:52 +0300

    Initial Commit

---
+++ UperCloneUdemy/UperCloneUdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UperCloneUdemyApp.swift
+//  UperCloneUdemy
+//
+//  Created by Tariq Maged on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct UperCloneUdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
